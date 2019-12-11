546707a66f9986a5f5145778c0ec5473f8e7b220
Paul Brit <paulbrit44@gmail.com>
2019-12-11 13:06:48 -0700

    Add musixmatch cask v3.6.1

---
+++ Casks/musixmatch.rb
@@ -0,0 +1,10 @@
+cask 'musixmatch' do
+    version '3.6.1'
+    sha256 '877a63a741805f6e239201158520973a411b7efaf5e599188e9d0891d38c151d'
+  
+    url "https://download-app.musixmatch.com/download/Musixmatch-#{version}.dmg"
+    name 'Musixmatch'
+    homepage 'https://musixmatch.com/'
+  
+    app ''
+  end
